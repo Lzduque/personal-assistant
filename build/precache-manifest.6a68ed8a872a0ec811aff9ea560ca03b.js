a52b98af0def3a42f2514091bc544de73dbba1f3
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2019-06-16 20:56:15 -0400

    Add files via upload

---
+++ build/precache-manifest.6a68ed8a872a0ec811aff9ea560ca03b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "82ffd3f41c7ddce7d1d4",
+    "url": "/static/js/main.1d2047a4.chunk.js"
+  },
+  {
+    "revision": "b510e97e61c1caf86506",
+    "url": "/static/js/2.4eeb55b6.chunk.js"
+  },
+  {
+    "revision": "82ffd3f41c7ddce7d1d4",
+    "url": "/static/css/main.74cd953e.chunk.css"
+  },
+  {
+    "revision": "b510e97e61c1caf86506",
+    "url": "/static/css/2.74b84a41.chunk.css"
+  },
+  {
+    "revision": "95bc59f85ab3a770dcfca04fcd98497e",
+    "url": "/index.html"
+  }
+];
